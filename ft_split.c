5bb4a9709578c988562a344b7658078e93a10da6
ridalgo- <ridalgo-@student.42sp.org.brr>
2022-04-25 01:20:47 +0200

    norminette changes

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/23 20:47:14 by ridalgo-          #+#    #+#             */
-/*   Updated: 2022/04/25 00:38:38 by ridalgo-         ###   ########.fr       */
+/*   Updated: 2022/04/25 01:01:26 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,26 +34,15 @@ static int	ft_count_words(const char *s, char c)
 	return (count);
 }
 
-char	**ft_split(char const *s, char c)
+void	ft_wtfunction(char **ans, int word, char c, char *aux)
 {
-	char	**ans;
-	char	*aux;
-	int		word;
-	int		i;
-	int		j;
-	int		start;
-	int		end;
-	int		size;
+	int	i;
+	int	j;
+	int	start;
+	int	end;
 
 	i = 0;
 	j = 0;
-	aux = ft_strtrim(s, &c);
-	if (!aux && aux[i] == 0)
-		return (NULL);
-	word = ft_count_words(s, c);
-	ans = (char **)malloc(sizeof(char *) * (word + 1));
-	if (!ans)
-		return (NULL);
 	while (i < word)
 	{
 		while (aux[j] == c)
@@ -65,10 +54,25 @@ char	**ft_split(char const *s, char c)
 				end = j;
 			j++;
 		}
-		size = end - start + 1;
-		ans[i] = ft_substr(&aux[start], 0, size);
+		ans[i] = ft_substr(&aux[start], 0, (end - start + 1));
 		i++;
 	}
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**ans;
+	char	*aux;
+	int		word;
+
+	aux = ft_strtrim(s, &c);
+	if (!aux && aux[0] == 0)
+		return (NULL);
+	word = ft_count_words(s, c);
+	ans = (char **)malloc(sizeof(char *) * (word + 1));
+	if (!ans)
+		return (NULL);
+	ft_wtfunction(ans, word, c, aux);
 	ans[word] = NULL;
 	free(aux);
 	return (ans);
